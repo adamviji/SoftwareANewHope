fbce68c0f8ba19dc82c1f9b0fcefaa658357470a
adamviji <adam.visanji@gmail.com>
2026-03-03 14:45:28 +0100

    Revert "make some testfiles"
    This reverts commit 9a1f96d2370a6c9f3b03b1e4d68a34d54ac21851.

--- Software_test/test.cpp
+++ Software_test/test.cpp
@@ -1,24 +1,6 @@
 #include "pch.h"
-#include "../Software_lib/Cat.h"
 
 TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
-}
-
-class CatTests_fixture : public testing::Test {
-protected:
-	Cat myCat;
-
-	void SetUp() override {}
-	void TearDown() override {}
-
-public:
-	CatTests_fixture() {}
-	~CatTests_fixture() {}
-
-};
-
-TEST_F(CatTests_fixture, def_constr_test) {
-	ASSERT_EQ(myCat.getAge(), 42);
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
 }
