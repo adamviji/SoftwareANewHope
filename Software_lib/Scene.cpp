31d62da25eb69ecbcfa451375635e8d41b426d73
adamviji <adam.visanji@gmail.com>
2026-03-05 21:20:10 +0100

    Scene h/cpp done

---
+++ Software_lib/Scene.cpp
@@ -0,0 +1,45 @@
+#include "Scene.h"
+
+Scene::Scene(std::string sceneName)
+{
+	this->sceneName = sceneName;
+}
+
+Scene::~Scene()
+{
+}
+
+std::string Scene::getName()
+{
+	return this->sceneName;
+}
+
+void Scene::addGameObject(std::unique_ptr<GameObject> obj)
+{
+	Objects.push_back(std::move(obj));
+}
+
+std::string Scene::listAvailableElements() const
+{
+	std::string result;
+	for (size_t i = 0; i < Objects.size(); i++)
+	{
+		result += Objects[i]->getName();
+		if (i + 1 < Objects.size()) result += ", ";
+	}
+	return result;
+}
+
+GameObject* Scene::getGameObject(const std::string& Name)
+{
+	for (auto& obj : Objects)
+		if (obj->getName() == Name) return obj.get();
+	return nullptr;
+}
+
+bool Scene::isAvailable(const std::string& name) const
+{
+	for (auto& obj : Objects)
+		if (obj->getName() == sceneName) return true;
+	return false;
+}
--- Software_lib/Scene.h
+++ Software_lib/Scene.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "GameObject.h"
 
+
 class Scene {
 private:
-	std::vector<std::unique_ptr<GameObject>> availableObjects;
+	std::vector<std::unique_ptr<GameObject>> Objects;
 	std::string sceneName;
 
 public:
-	Scene();
+	Scene(std::string sceneName);
 	~Scene();
 
+	std::string getName();
+	void addGameObject(std::unique_ptr<GameObject> obj);
+	std::string listAvailableElements() const;
+	GameObject* getGameObject(const std::string& sceneName);
+	bool isAvailable(const std::string& name) const;
 
 };
