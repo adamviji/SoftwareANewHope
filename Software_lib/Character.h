a683ddb1b7a8222a78e3cf7f3c7db6556a8842ca
adamviji <adam.visanji@gmail.com>
2026-03-06 23:46:31 +0100

    Create Character.h

---
+++ Software_lib/Character.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+class Character
+{
+private:
+	std::string name;
+	std::string greeting;
+
+public:
+	Character(std::string name, std::string greeting) {
+		this->name = name;
+		this->greeting = greeting;
+	}
+
+	std::string getName() {
+		return this->name;
+	}
+	std::string getInitialGreeting() {
+		return this->greeting;
+	}
+	std::string talk(std::string message) {
+		return this->name + " says: " + message;
+	}
+};
